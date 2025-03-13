213b50bae56639db0715329e1fe05c88d642303e
Your Name <kamesco.pe60@gmail.comcd>
2025-03-13 18:41:03 +0100

    Initial Commit

---
+++ Supercold/SupercoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupercoldApp.swift
+//  Supercold
+//
+//  Created by Oumar Ka on 13/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SupercoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SupercoldTests/SupercoldTests.swift
@@ -0,0 +1,17 @@
+//
+//  SupercoldTests.swift
+//  SupercoldTests
+//
+//  Created by Oumar Ka on 13/03/2025.
+//
+
+import Testing
+@testable import Supercold
+
+struct SupercoldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
